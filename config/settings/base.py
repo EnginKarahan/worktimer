792a593cd33e7c919446ea477c0fac9f86fcba88
EnginKarahan <engin.karahan@gmail.com>
2026-05-18 15:23:23 +0200

    Fix Mailproblem

--- config/settings/base.py
+++ config/settings/base.py
@@ -152,6 +152,8 @@ FEDERAL_STATE = config("FEDERAL_STATE", default="BY")
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = config("EMAIL_HOST", default="localhost")
 EMAIL_PORT = config("EMAIL_PORT", default=25, cast=int)
+EMAIL_USE_TLS = config("EMAIL_USE_TLS", default=False, cast=bool)
+EMAIL_USE_SSL = config("EMAIL_USE_SSL", default=False, cast=bool)
 EMAIL_HOST_USER = config("EMAIL_HOST_USER", default="")
 EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD", default="")
 DEFAULT_FROM_EMAIL = config("DEFAULT_FROM_EMAIL", default="arbeitszeit@example.com")
